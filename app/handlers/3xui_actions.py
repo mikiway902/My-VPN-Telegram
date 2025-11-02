5d6df9583f5cf48c3ad84ac99b282e105d86140c
mikiway <sladkov.mikhail@gmail.com>
2025-11-02 10:52:38 +0300

    new additionals to code

--- app/handlers/3xui_actions.py
+++ app/handlers/3xui_actions.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 import DataBaseLib
-from py3xui import AsyncApi
+import py3xui
 
 # Загрузка .env
 env_path = Path(__file__).resolve().parent / ".env"
@@ -24,5 +24,25 @@ XUI_HOST_FULL = str(f"http://{str(XUI_HOST)}:{str(XUI_PORT)}")
 print(env_path)
 print(XUI_HOST_FULL)
 
+async def login_3xui(XUI_HOST_FULL, XUI_USERNAME, XUI_PASSWORD):
+    api = py3xui.AsyncApi(XUI_HOST_FULL, XUI_USERNAME, XUI_PASSWORD)
+    await api.login()
+    settings = Settings()
+    sniffing = Sniffing(enabled=True)
 
-api = AsyncApi(XUI_HOST_FULL, XUI_USERNAME, XUI_PASSWORD)
+    tcp_settings = {
+        "acceptProxyProtocol": False,
+        "header": {"type": "none"},
+    }
+    stream_settings = StreamSettings(security="reality", network="tcp", tcp_settings=tcp_settings)
+
+    inbound = Inbound(
+        enable=True,
+        port=443,
+        protocol="vless",
+        settings=settings,
+        stream_settings=stream_settings,
+        sniffing=sniffing,
+        remark="test3",
+    )
+    await api.inbound.add(inbound)
--- app/handlers/TgBotActions.py
+++ app/handlers/TgBotActions.py
@@ -1,4 +1,4 @@
-from aiogram import Router, types, F
+from aiogram import Router, types, F, html
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.context import FSMContext
 from datetime import datetime
@@ -6,7 +6,7 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 
-from handlers.DataBaseLib import add_user_db, show_user_db
+from handlers import DataBaseLib as db
 
 # Загрузка .env
 env_path = Path(__file__).resolve().parent / ".env"
@@ -21,6 +21,7 @@ router = Router()
 
 class RegistrationForm(StatesGroup):
     email = State()
+    
 
 @router.message(F.text == "Регистрация")
 async def start_registration(message: types.Message, state: FSMContext):
@@ -36,17 +37,28 @@ async def get_email(message: types.Message, state: FSMContext):
     sender_id = data["sender_id"]
     email = data["email"]
 
-    await add_user_db(POSTGRES_DB, DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, sender_id, email)
+    await db.add_user_db(POSTGRES_DB, DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, sender_id, email)
     await message.answer(f"Email {email} сохранён ✅")
     await state.clear()
 
 @router.message(F.text == "Мои данные")
 async def return_user_data(message: types.Message):
     sender_id = message.from_user.id
-    user = await show_user_db(POSTGRES_DB, DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, sender_id)
+    user = await db.show_user_db(POSTGRES_DB, DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, sender_id)
 
     if user:
         user_text = "\n".join(f"{k}: {v}" for k, v in user.items())
         await message.answer(f"Ваши данные:\n\n{user_text}")
     else:
         await message.answer("Пользователь не найден 😢")
+        
+@router.message(F.text == "Удалить VPN")
+async def delete_user_data(message: types.Message):
+    sender_id = message.from_user.id
+    await db.delete_toogle_user_db(POSTGRES_DB, DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, sender_id)
+    await message.answer(f"Для пользователя {html.bold(message.from_user.full_name)} профиль удалён. ВПН остановлен")
+
+@router.message(F.text == "Установить VPN")
+async def installVPN(message: types.message, state: FSMContext):
+    sender_id = message.from_user.id
+    await db.
